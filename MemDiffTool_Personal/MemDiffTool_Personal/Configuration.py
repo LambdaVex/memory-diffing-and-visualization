b7faf4ee87ca244ad69ecab20c1ded6eef055088
mhddenno <dennokhmhd@gmail.com>
2016-12-17 17:55:09 +0100

    Delete Configuration.py

--- MemDiffTool_Personal/MemDiffTool_Personal/Configuration.py
+++
@@ -1,3 +0,0 @@
-dump_memory_location=r"D:\Dump\A\dumpMemFile.mem"
-volatility_standalone_location=r"D:\volatility_2.5.win.standalone\volatility_2.5.win.standalone\volatility-2.5.standalone.exe"
-output_location=r"C:\Users\Denno\Desktop\trip"
