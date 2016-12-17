9f768348e84c19c63c5db1b292e4f71d24233009
Ali Denno <aliedenno@gmail.com>
2016-12-17 17:44:13 +0100

    Update gitignro

--- MemDiffTool_Personal/MemDiffTool_Personal/Configuration.py
+++ MemDiffTool_Personal/MemDiffTool_Personal/Configuration.py
@@ -1,3 +1,4 @@
+# Directories
 dump_memory_location=r"C:\Users\ali-d\Desktop\Work\MEMFILE2GB\dumpMemFile.mem"
 volatility_standalone_location=r"C:\Users\ali-d\Dropbox\Universities\University of Bonn\Semester 3\Lab ITSecurity\MemDiff_Personal\MemDiffLab\MemDiff\MemDiffTool_Personal\MemDiffTool_Personal\volatility_2.5.win.standalone\volatility-2.5.standalone.exe"
 output_location=r"C:\Users\ali-d\Desktop\Programming\DumpInfo"
