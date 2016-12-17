d12685ebdb135eb9aa906eefe91beae026010439
Ali Denno <aliedenno@gmail.com>
2016-12-17 17:26:38 +0100

    Update: Gitignore and clean up

--- MemDiffTool_Personal/MemDiffTool_Personal/Configuration.py
+++ MemDiffTool_Personal/MemDiffTool_Personal/Configuration.py
@@ -1,3 +1,3 @@
-dump_memory_location="D:\Dump\A\dump.mem"
-volatility_standalone_location="D:\volatility_2.5.win.standalone\volatility_2.5.win.standalone\volatility-2.5.standalone.exe"
-output_location="DumpInfo\memmap"
+dump_memory_location=r"C:\Users\ali-d\Desktop\Work\MEMFILE2GB\dumpMemFile.mem"
+volatility_standalone_location=r"C:\Users\ali-d\Dropbox\Universities\University of Bonn\Semester 3\Lab ITSecurity\MemDiff_Personal\MemDiffLab\MemDiff\MemDiffTool_Personal\MemDiffTool_Personal\volatility_2.5.win.standalone\volatility-2.5.standalone.exe"
+output_location=r"C:\Users\ali-d\Desktop\Programming\DumpInfo"
