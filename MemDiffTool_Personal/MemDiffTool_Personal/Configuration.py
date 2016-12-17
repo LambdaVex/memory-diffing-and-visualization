bdae4524510a4c38cb3b5c5c7806246e1bc4d5c2
Ali Denno <aliedenno@gmail.com>
2016-12-17 17:41:16 +0100

    Update: TEST

--- MemDiffTool_Personal/MemDiffTool_Personal/Configuration.py
+++ MemDiffTool_Personal/MemDiffTool_Personal/Configuration.py
@@ -1,3 +1,3 @@
-dump_memory_location=r"D:\Dump\A\dumpMemFile.mem"
-volatility_standalone_location=r"D:\volatility_2.5.win.standalone\volatility_2.5.win.standalone\volatility-2.5.standalone.exe"
-output_location=r"C:\Users\Denno\Desktop\trip"
+dump_memory_location=r"C:\Users\ali-d\Desktop\Work\MEMFILE2GB\dumpMemFile.mem"
+volatility_standalone_location=r"C:\Users\ali-d\Dropbox\Universities\University of Bonn\Semester 3\Lab ITSecurity\MemDiff_Personal\MemDiffLab\MemDiff\MemDiffTool_Personal\MemDiffTool_Personal\volatility_2.5.win.standalone\volatility-2.5.standalone.exe"
+output_location=r"C:\Users\ali-d\Desktop\Programming\DumpInfo"
