c7cba7b39988367384107595799ac3987488c314
mhddenno <dennokhmhd@gmail.com>
2016-12-25 22:47:06 +0100

    Update module.py

--- MemDiffTool_Personal/MemDiffTool_Personal/module.py
+++ MemDiffTool_Personal/MemDiffTool_Personal/module.py
@@ -7,6 +7,7 @@ from pandas import DataFrame
 import bisect_module as bi
 import time
 import page as pg
+import mapping
 class Module:
 
     def __init__(self, name, base,size):
@@ -20,13 +21,16 @@ class Module:
         print(pid+" "+self.base)
         memmap=pandas.read_fwf(co.output_location+"\memmap_"+pid+".info",widths=[18,18,18,18])
         virtual_address=memmap.ix[:,0]
+        physical_address=memmap.ix[:,1]
+        #page_size=memmap.ix[:,2].replace(" ", "")
         #print(virtual_address)
         page=bi.index(virtual_address,self.base)
         print(page)
        # print(virtual_address[page])
         if(page != -1):
             while int(virtual_address[page],16)<=int(self.base,16)+int(self.size,16):
-                newPage=pg.Page(virtual_address[page],"print_Ascci","non_print_Ascci","print_Ascci_Num")
+                statistics=mapping.slicing(physical_address[page],'0x1000')
+                newPage=pg.Page(virtual_address[page],statistics[0],statistics[1],statistics[2])
                 self.pages.append(virtual_address[page])
                 print(virtual_address[page]+"Added!")
                 page=page+1
