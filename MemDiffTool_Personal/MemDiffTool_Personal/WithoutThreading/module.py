0edd8244ad7f0ec1650558a75d3468e21fb0551f
Mohamad Denno <dennokhmhd@gmail.com>
2017-02-18 13:26:21 +0100

    new structure

---
+++ MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/module.py
@@ -0,0 +1,45 @@
+import subprocess
+import os
+import pandas
+import configuration as co
+from bisect import *
+from pandas import DataFrame
+import bisect_module as bi
+import time
+import page as pg
+import mapping
+class Module:
+
+    def __init__(self, name, base, size):
+        self.name = name
+        self.base = base
+        self.size = size
+        self.memory_used_by_pages=0
+        self.pages = []    
+        self.hmpages = [] 
+        #252
+
+    def add_pages(self,pid):
+        #print(pid+" "+self.base)
+        memmap=pandas.read_fwf(co.output_location+"\memmap_"+pid+".info",colspecs=[(0,18),(19,37),(38,56),(57,75)])
+        virtual_address=memmap.ix[:,0]
+        physical_address=memmap.ix[:,1]
+        #the size of memory used
+        sizes=memmap.ix[:,2]
+        #the sum of pages sizes
+        #self.memory_used_by_pages=sum(int(i,16) for i in sizes[2:])
+        #page_size=memmap.ix[:,2].replace(" ", "")
+        #print(virtual_address)
+        page=bi.index(virtual_address,self.base)
+  
+        if(page != -1):
+            while int(virtual_address[page],16)<=int(self.base,16)+int(self.size,16):
+                #MHDCODE
+                statistics=mapping.slicing(physical_address[page],sizes[page])
+                #newPage=pg.Page(virtual_address[page],statistics[0],statistics[1],statistics[2])
+                newPage=pg.Page(virtual_address[page],statistics[0],statistics[1],statistics[2],statistics[3],sizes[page])
+                self.pages.append(newPage)
+                #self.pages.append(virtual_address[page])
+                #print(virtual_address[page]+"Added!")
+                page=page+1
+        #time.sleep(5)
---
+++ MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/volatilityInteraction.py
@@ -0,0 +1,89 @@
+import subprocess
+import os
+import pandas
+import configuration as co
+
+volatilityLoc=co.volatility_standalone_location
+dumpLoc=co.dump_memory_location
+
+def vol_pslist():
+    f = open(co.output_location+"\pslist.info", "w")
+    Command=volatilityLoc+" -f "+dumpLoc+" --profile=Win7SP1x64 pslist"
+    subprocess.call(Command,stdout=f)
+    print("pslist done!")
+
+def vol_memmap(PID):
+    f = open(co.output_location+"\memmap"+"_"+str(PID)+".info", "w")
+    Command=volatilityLoc+" -f "+dumpLoc+" --profile=Win7SP1x64 memmap -p " + str(PID)
+    subprocess.call(Command,stdout=f)
+    print("memmap for PID:"+str(PID)+" is done!")
+
+def vol_dlllist(PID):
+    f = open(co.output_location+"\dlllist"+"_"+str(PID)+".info", "w")
+    Command=volatilityLoc+" -f "+dumpLoc+" --profile=Win7SP1x64 dlllist -p " + str(PID)
+    subprocess.call(Command,stdout=f)
+    print("dlllist for PID:"+str(PID)+" is done!")
+
+def vol_memdump(PID):
+    f = open(co.output_location+"\memdump"+"_"+str(PID)+".info", "w")
+    Command=volatilityLoc+" -f "+dumpLoc+" --profile=Win7SP1x64 memdump -p " + str(PID) +" -D "+co.output_location+"\DumpFolder"
+    subprocess.call(Command,stdout=f)
+    print("memdump for PID:"+str(PID)+" is done!")
+
+
+def vol_new(PID):
+    f = open(co.output_location+"\memdump"+"_"+str(PID)+".info", "w")
+    Command=volatilityLoc+" -f "+dumpLoc+" --profile=Win7SP1x64 memdump -p " + str(PID) +" -D "+co.output_location+"\DumpFolder"
+    subprocess.call(Command,stdout=f)
+    print("memdump for PID:"+str(PID)+" is done!")
+
+def processMemory(infile):
+    df3=pandas.read_fwf(infile)
+    for i in range(len(df3)):   
+        if(df3.iloc[i,0][0]=='0'):
+            #print(df3.iloc[i,0][0])
+            hexVal=df3.iloc[i,0][2:]
+            decVal=int(hexVal, 16)
+            bitVal=bin(decVal)
+           # print("{0}".format(bitVal))
+            print("{0} --> {1} --> {2}".format(df3.iloc[i,0],decVal,bitVal))
+
+
+# First Obtain pslist
+vol_pslist()
+pslist=pandas.read_fwf(co.output_location+"\pslist.info")
+#pslist=pandas.read_fwf('DumpInfo\pslist.info')
+
+# Print your PID list
+#for i in range(len(pslist)):
+#    print("{0}".format(pslist.iloc[i,2]))
+
+#vol_new(280)
+
+for i in range(1,len(pslist)):
+    vol_dlllist(pslist.iloc[i,2])
+    vol_memmap(pslist.iloc[i,2])
+    #vol_memdump(pslist.iloc[i,2])
+#for i in range(1,len(pslist)):
+#    vol_memmap(pslist.iloc[i,2])
+    #print("{0}".format(pslist.iloc[i,2]))
+#vol_memmap(4)
+
+'''
+file = open('memmap.txt', 'r')
+content=file.read()
+#print (content)
+df3=pandas.read_fwf('memmap.txt')
+
+myDict=dict()
+#print(len(df3))
+for i in range(len(df3)):
+    #print("{0} -- {1}".format(df3.iloc[i,1],df3.iloc[i,2]))
+    #myDict.update({df3.iloc[i,1]: df3.iloc[i,2]})
+    myDict[df3.iloc[i,1]]=df3.iloc[i,2]
+
+keylist = myDict.keys()
+keylist.sort()
+for i in range(len(keylist)):
+    print ("{0} --- {1}".format(keylist[i],int(keylist[i], 16)))
+'''
