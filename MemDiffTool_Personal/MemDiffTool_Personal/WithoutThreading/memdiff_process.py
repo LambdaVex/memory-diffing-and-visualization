f4dff8706d245a089c26a396bee91759220ba250
Mohamad Denno <dennokhmhd@gmail.com>
2017-02-19 16:38:33 +0100

    nothing happened

--- MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/memdiff_process.py
+++ MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/memdiff_process.py
@@ -14,14 +14,14 @@ start_time = time.time()
 ######## Initialize the Dump file
 dump1 = md.MemoryDump(co.dump_memory_location)
 ######## Cash the processes of the Dump file
-#dump1.vol_pslist() #Uncomment for New Dumps
-dump1.cashing_of_processes()
+dump1.vol_pslist() #Uncomment for New Dumps
+#dump1.cashing_of_processes()
 ######## Cash the modules of each process
-# dump1.vol_dlllist() #Uncomment for New Dumps
-dump1.cashing_of_modules()
+dump1.vol_dlllist() #Uncomment for New Dumps
+#dump1.cashing_of_modules()
 ######## Cash the pages of each module
-#dump1.vol_memmap() #Uncomment for New Dumps
-dump1.cashing_of_pages()
+dump1.vol_memmap() #Uncomment for New Dumps
+#dump1.cashing_of_pages()
 
-xmlb.build_file(dump1)
+#xmlb.build_file(dump1)
 #xmlp.prase()
