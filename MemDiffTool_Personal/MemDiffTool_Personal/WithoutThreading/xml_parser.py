8a9c56c8c059119629025840920b71028f3a7cac
Mohamad Denno <dennokhmhd@gmail.com>
2017-02-28 13:25:16 +0100

    fix the bugs

--- MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/xml_parser.py
+++ MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/xml_parser.py
@@ -58,7 +58,6 @@ def parse(heatmap_dump,filename):
                 tpage=pge.Page(root[i][j][k].text,root[i][j][k].attrib['Asci'],root[i][j][k].attrib['NAsci'],root[i][j][k].attrib['Num'],root[i][j][k].attrib['Ent'],root[i][j][k].attrib['Size'],root[i][j][k].attrib['Hash'],root[i][j][k].attrib['RelativeOffset'])
                 heatmap_dump.processes[i].modules[j].pages.append(tpage)
 
-
         heatmap_dump.processes[i].sum_sizes=sum_sizes
     return heatmap_dump
 def truncate(f, n):
