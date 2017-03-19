f49c17737996cd49dd1168fdc8d2aeb22a2269dd
Mohamad Denno <dennokhmhd@gmail.com>
2017-03-19 13:39:32 +0100

    not very important

--- MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/xml_builder.py
+++ MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/xml_builder.py
@@ -16,7 +16,7 @@ def build_file(dump1):
                 #round
     tree = ET.ElementTree(data)
     #to remove threading uncomment the line below and comment the one below it
-    tree.write("WithMalware.xml")
+    tree.write("withMalware.xml")
     
 def combine_xml(files):
     first = None
