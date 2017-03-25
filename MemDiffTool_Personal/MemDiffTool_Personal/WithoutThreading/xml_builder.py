5b95f37d40770201ff4077a1eba0433f6e86051e
Ali Denno <aliedenno@gmail.com>
2017-03-25 08:35:54 +0100

    Update builder

--- MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/xml_builder.py
+++ MemDiffTool_Personal/MemDiffTool_Personal/WithoutThreading/xml_builder.py
@@ -16,7 +16,7 @@ def build_file(dump1):
                 #round
     tree = ET.ElementTree(data)
     #to remove threading uncomment the line below and comment the one below it
-    tree.write("withMalwareNEW.xml")
+    tree.write("FirshCache.xml")
     
 def combine_xml(files):
     first = None
